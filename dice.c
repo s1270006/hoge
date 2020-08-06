7ce3aa018b4a814911a9de2990c3093c39d155cb
kosei kasahara <[s1270006@u-aizu.ac.jp]>
2020-08-06 22:31:42 +0900

    Greetings added

--- dice.c
+++ dice.c
@@ -2,9 +2,26 @@
 #include<time.h>
 #include<stdlib.h>
 
+#define NAME_MAX 30
+
 int main(){
   
   int i, heads = 0, tails = 0;
+  char name[NAME_MAX];
+  
+  printf("Who are you?\n>");
+  
+  for(i = 0; i < NAME_MAX; i++)
+  {
+    scanf("%c",&name[i]);
+    if(name[i] == '\n'){
+      name[i] = '!';
+      for(i = i + 1; i < NAME_MAX; i++){
+        name[i] = '\0';
+      }
+    }
+  }
+  printf("Hello, %s\n",name);
   
   printf("Tossing a coin...\n");
   srand((unsigned)time(NULL));
